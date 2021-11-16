af8f5d65ad0a153c92946fb7dd2a1609e251be41
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-16 09:40:20 -0500

    Implemented Form

---
+++ src/Form/Form.tsx
@@ -0,0 +1,44 @@
+import { useRef } from 'react'
+import { Formik, FormikConfig, FormikHelpers } from 'formik'
+import { ConcreteFormProvider, ConcreteFormProps } from '@concrete-form/core'
+import FormikFormHandler from '../FormikFormHandler'
+
+export type FormProps<Values> = {
+  initialValues: Values
+  onSubmit?: (values: Values, formikHelpers?: FormikHelpers<Values>) => void | Promise<void>
+  formikProps?: Omit<FormikConfig<any>, 'initialValues'|'onSubmit'>
+} & ConcreteFormProps
+
+const Form: React.FC<FormProps<any>> = ({
+  onSubmit = () => {},
+  initialValues,
+  formikProps,
+  noValidate = true,
+  formProps,
+  children,
+  ...concreteFormConfig
+}) => {
+  const formHandler = useRef(new FormikFormHandler())
+
+  return (
+    <Formik onSubmit={onSubmit} initialValues={initialValues} {...formikProps}>
+      { updatedFormikProps => {
+        formHandler.current.setFormikProps(updatedFormikProps)
+
+        return (
+          <ConcreteFormProvider formHandler={formHandler.current} config={concreteFormConfig}>
+            <form
+              onSubmit={updatedFormikProps.handleSubmit}
+              noValidate={noValidate}
+              {...formProps}
+            >
+              { children }
+            </form>
+          </ConcreteFormProvider>
+        )
+      } }
+    </Formik>
+  )
+}
+
+export default Form
