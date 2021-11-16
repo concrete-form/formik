b7e8751505777037588d6cb88bfaa9a0b485c54f
Kevin Gilbert <kevin@kegisiteweb.com>
2021-11-16 09:40:29 -0500

    Added tests for Form

---
+++ src/Form/Form.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import Form from './Form'
+
+// using "any" as a workaround for a bug in typescript : https://github.com/microsoft/TypeScript/issues/28960
+const testIdForm = { formProps: { 'data-testid': 'form' } } as any
+
+describe('Form', () => {
+  it('render novalidate by default', () => {
+    render(<Form {...testIdForm} />)
+    expect(screen.getByTestId('form')).toHaveProperty('noValidate', true)
+  })
+
+  it('doesn\'t render novalidate when disabled', () => {
+    render(<Form {...testIdForm} noValidate={false} />)
+    expect(screen.getByTestId('form')).toHaveProperty('noValidate', false)
+  })
+
+  it('renders children', () => {
+    render(<Form onSubmit={jest.fn()} initialValues={{}}>test-children</Form>)
+    expect(screen.getByText('test-children')).toBeInTheDocument()
+  })
+
+  it('calls onSubmit', async () => {
+    const onSubmit = jest.fn()
+    render(<Form onSubmit={onSubmit} initialValues={{}}><button type="submit">submit</button></Form>)
+    userEvent.click(screen.getByRole('button', { name: 'submit' }))
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled()
+    })
+  })
+
+  it('doesn\'t crash without onSubmit callback', async () => {
+    render(<Form initialValues={{}}><button type="submit">submit</button></Form>)
+    const submitButton = screen.getByRole('button', { name: 'submit' })
+    userEvent.click(submitButton)
+
+    await waitFor(() => {
+      expect(submitButton).toBeEnabled()
+    })
+  })
+})
